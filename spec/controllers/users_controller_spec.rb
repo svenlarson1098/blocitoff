1e749dd9a0ca0cfeae0a3adc175f3b51723951c7
svenlarson1098 <svenlarson1098@gmail.com>
2018-09-12 21:49:48 -0700

    Checkpoint 3 Complete

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show
+      expect(response).to have_http_status(:success)
+    end
+
+    it "instantiates a new user" do
+      get :show
+      expect(assigns(:users)).to_not be_nil
+    end
+end
+end
