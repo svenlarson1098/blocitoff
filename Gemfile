e4d85be356fc888f72bd1441ec9c7f3b5550606c
svenlarson1098 <svenlarson1098@gmail.com>
2018-09-16 21:54:36 -0700

    Checkpoint 5 Complete

--- Gemfile
+++ Gemfile
@@ -62,5 +62,6 @@ end
 
 gem 'devise'
 gem 'figaro'
+gem 'faker'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+#Users created
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+#items created
+10.times do
+  Item.create!(
+    name: Faker::DumbAndDumber.quote,
+    user: users.sample
+    )
+  end
+
+puts "Seeding is Finished."
+puts "#{User.count} users created."
+puts "#{Item.count} items created."
